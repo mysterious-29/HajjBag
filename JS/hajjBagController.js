cd708fa14f05bde688721ceef7d8424e83ed0b46
Nikhil Agrawal <nikhilgtech@gmail.com>
2017-07-17 07:19:42 +0530

    image click routing fixed

--- JS/hajjBagController.js
+++ JS/hajjBagController.js
@@ -1,7 +1,9 @@
 angular.module('HajjBagsApp').controller('HajjAppController', ['$scope','dataShare','$location','$http', function($scope,dataShare,$location,$http){
-	$scope.ShowItem=function(){
-		var product={};
+	var product={};
+	$scope.ShowItem=function(product){
+		
 		dataShare.setChoice(product);
+		$scope.$broadcast('selectionChanged',product);
 		$location.path('/item');
 			
 	}
